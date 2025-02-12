892ccc9e41d43c21cd69aaa3fcb1a619c0764313
Saleh545 <syusifzade545@gmail.com>
2025-02-13 01:45:41 +0400

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import girl from "../../public/images/girl.png";
-import { FaArrowsAltV, FaFemale, FaRunning, FaWheelchair } from "react-icons/fa";
+import { FaArrowsAltV, FaFemale, FaRunning } from "react-icons/fa";
 import bckimg from "../../public/images/hero.png";
+import { gsap } from "gsap";
+
 const Home = () => {
+  useEffect(() => {
+    // h2 animasyonu
+    gsap.from(".hero-caption h2", {
+      x: -200, 
+      opacity: 0,
+      duration: 1,
+    });
+    
+    // h5 animasyonu, h2'den 0.3 saniye sonra başlasın
+    gsap.from(".hero-caption h5", {
+      x: -200,
+      opacity: 0,
+      duration: 1,
+      delay: 0.3,
+    });
+    
+    // a animasyonu, h5'ten 0.3 saniye sonra başlasın
+    gsap.from(".hero-caption a", {
+      x: -200,
+      opacity: 0,
+      duration: 1,
+      delay: 0.6,
+    });
+  }, []);
+
   return (
     <div>
       <div className="home">
@@ -25,8 +52,7 @@ const Home = () => {
           <div className="all-flex">
             <div className="left">
               <img src={girl} alt="" />
-              <h2>All About
-Fitness</h2>
+              <h2>All About Fitness</h2>
             </div>
             <div className="right">
               <div className="info-text">
@@ -42,20 +68,24 @@ Fitness</h2>
                 </div>
               </div>
               <div className="info-text">
-              <FaRunning />
+                <FaRunning />
                 <div className="text">
                   <h5>Running</h5>
                   <p>
-                  Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet…
+                    Running is a method of terrestrial locomotion allowing humans
+                    and other animals to move rapidly on foot. Running is a type
+                    of gait characterized by an aerial phase in which all feet...
                   </p>
                 </div>
               </div>
               <div className="info-text">
-              <FaFemale />
+                <FaFemale />
                 <div className="text">
                   <h5>Yoga</h5>
                   <p>
-                  Yoga, is a meditative means of discovering dysfunctional perception and cognition, as well as overcoming it for release from suffering, inner peace and salvation
+                    Yoga, is a meditative means of discovering dysfunctional
+                    perception and cognition, as well as overcoming it for release
+                    from suffering, inner peace and salvation
                   </p>
                 </div>
               </div>
