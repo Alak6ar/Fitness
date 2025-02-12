5b31623afbcc02673396a21c0fd98b72d599d320
Saleh545 <syusifzade545@gmail.com>
2025-02-12 17:31:02 +0400

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../../public/images/logo.jpg";
+import { Link } from "react-router-dom";
+import { IoSearchSharp } from "react-icons/io5";
+import { SlBasket } from "react-icons/sl";
+import Hamburger from "hamburger-react";
 
 const Header = () => {
-  return (
-    <div>
-        <h1>salaaam</h1>
-    </div>
-  )
-}
+  const [isOpen, setOpen] = useState(false);
 
-export default Header
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        
+        {/* Hamburger Menü */}
+      
+
+        {/* Navbar */}
+        <div className={`navbar ${isOpen ? "active" : ""}`}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/pages">Pages</Link>
+            </li>
+            <li>
+              <Link to="/elements">Elements</Link>
+            </li>
+            <li>
+              <Link to="/classes">Classes</Link>
+            </li>
+            <li>
+              <Link to="/schedule">Schedule</Link>
+            </li>
+            <li>
+              <Link to="/trainers">Trainers</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Sağ taraf */}
+        <div className="right">
+            <div className="icons">
+
+          <div className="search">
+            <IoSearchSharp />
+          </div>
+          <div className="basket">
+            <SlBasket />
+          </div>
+            </div>
+          <div className="hamburger-menu">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import girl from "../../public/images/girl.png";
+import { FaArrowsAltV, FaFemale, FaRunning, FaWheelchair } from "react-icons/fa";
 
 const Home = () => {
   return (
     <div>
-        <Header/>
+      <div className="home">
+        <Header />
+        <div className="hero-caption">
+          <div className="container">
+            <h2>Hi This is Zacson</h2>
+            <h5>Gym Trainer</h5>
+            <Link>My courses</Link>
+          </div>
+        </div>
+      </div>
+      <div className="all-about">
+        <div className="">
+          <div className="all-flex">
+            <div className="left">
+              <img src={girl} alt="" />
+              <h2>All About
+Fitness</h2>
+            </div>
+            <div className="right">
+              <div className="info-text">
+                <FaArrowsAltV />
+                <div className="text">
+                  <h5>Weight Lifting</h5>
+                  <p>
+                    Weightlifting, also called Olympic weightlifting, is an
+                    athletic discipline in the modern Olympic programme in which
+                    the athlete attempts a maximum-weight single lift of a
+                    barbell loaded with weight plates.
+                  </p>
+                </div>
+              </div>
+              <div className="info-text">
+              <FaRunning />
+                <div className="text">
+                  <h5>Running</h5>
+                  <p>
+                  Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet…
+                  </p>
+                </div>
+              </div>
+              <div className="info-text">
+              <FaFemale />
+                <div className="text">
+                  <h5>Yoga</h5>
+                  <p>
+                  Yoga, is a meditative means of discovering dysfunctional perception and cognition, as well as overcoming it for release from suffering, inner peace and salvation
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
