594fa864a465b3e8b06f6ef31206dd1b2bdf0195
Saleh545 <syusifzade545@gmail.com>
2025-02-14 19:48:20 +0400

    a

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
 const Trainers = () => {
@@ -5,7 +6,7 @@ const Trainers = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
@@ -17,9 +18,155 @@ const Trainers = () => {
     <div>
       <div className="trainers">
         <div className="container">
+          <div className="trainers-up">
+            <h1>EXPERT TRAINERS</h1>
+            <div className="line"></div>
+          </div>
           <Slider {...settings}>
             <div>
-das
+              <div className="vc-item-wrap">
+                <div className="vc-item">
+                    <div className="trainer-img">
+                  <img src="https://radiustheme.com/demo/wordpress/gymedge/wp-content/uploads/2016/12/trainer_2.jpg" alt="" />{" "}
+                    </div>
+                  <div className="vc-overly">
+                    <ul>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                    </ul>
+                  </div>
+                  <div className="vc-team-meta">
+                    <h3><Link>Selina Stuart</Link></h3>
+               <div>Fitness Trainer</div>   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="vc-item-wrap">
+                <div className="vc-item">
+                    <div className="trainer-img">
+                  <img src="https://radiustheme.com/demo/wordpress/gymedge/wp-content/uploads/2016/12/trainer_2.jpg" alt="" />{" "}
+                    </div>
+                  <div className="vc-overly">
+                    <ul>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                    </ul>
+                  </div>
+                  <div className="vc-team-meta">
+                    <h3><Link>Selina Stuart</Link></h3>
+               <div>Fitness Trainer</div>   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="vc-item-wrap">
+                <div className="vc-item">
+                    <div className="trainer-img">
+                  <img src="https://radiustheme.com/demo/wordpress/gymedge/wp-content/uploads/2016/12/trainer_2.jpg" alt="" />{" "}
+                    </div>
+                  <div className="vc-overly">
+                    <ul>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                    </ul>
+                  </div>
+                  <div className="vc-team-meta">
+                    <h3><Link>Selina Stuart</Link></h3>
+               <div>Fitness Trainer</div>   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="vc-item-wrap">
+                <div className="vc-item">
+                    <div className="trainer-img">
+                  <img src="https://radiustheme.com/demo/wordpress/gymedge/wp-content/uploads/2016/12/trainer_2.jpg" alt="" />{" "}
+                    </div>
+                  <div className="vc-overly">
+                    <ul>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                    </ul>
+                  </div>
+                  <div className="vc-team-meta">
+                    <h3><Link>Selina Stuart</Link></h3>
+               <div>Fitness Trainer</div>   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="vc-item-wrap">
+                <div className="vc-item">
+                    <div className="trainer-img">
+                  <img src="https://radiustheme.com/demo/wordpress/gymedge/wp-content/uploads/2016/12/trainer_2.jpg" alt="" />{" "}
+                    </div>
+                  <div className="vc-overly">
+                    <ul>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                        <li>
+                            <Link></Link>
+                        </li>
+                    </ul>
+                  </div>
+                  <div className="vc-team-meta">
+                    <h3><Link>Selina Stuart</Link></h3>
+               <div>Fitness Trainer</div>   
+                  </div>
+                </div>
+              </div>
             </div>
           </Slider>
         </div>
