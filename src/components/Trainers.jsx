b8d48d96aa3eb5eb6e0c96a1d51e4f5cb823885e
Saleh545 <syusifzade545@gmail.com>
2025-02-14 18:41:59 +0400

    a

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import Slider from "react-slick";
 
 const Trainers = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
+      { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    ],
+  };
   return (
     <div>
-        <div className="trainers">
-            <div className="container">
-                
+      <div className="trainers">
+        <div className="container">
+          <Slider {...settings}>
+            <div>
+das
             </div>
+          </Slider>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Trainers
+export default Trainers;
