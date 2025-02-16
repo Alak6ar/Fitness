1cc1489d0d65e74b14f504b81d99b4949375f9f3
Saleh545 <syusifzade545@gmail.com>
2025-02-16 16:57:13 +0400

    a

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React from "react";
+import { FaCheckCircle } from "react-icons/fa";
 
 const Pricing = () => {
   return (
     <div>
-        <div className="pricing">
-            <div className="container">
-                <h1>OUR PRICING PLAN</h1>
+      <div className="pricing">
+        <div className="container">
+          <h1>OUR PRICING PLAN</h1>
+          <div className="pricing-flex">
+            <div className="pricing-card">
+              <h2>LEVEL 1</h2>
+              <p>$25</p>
+              <span>6 Week Package</span>
+              <ul>
+                <li><FaCheckCircle />Unlimited Gym Access</li>
+                <li><FaCheckCircle />6 Week Package</li>
+                <li><FaCheckCircle />12 - 18 Training Sessions</li>
+                <li><FaCheckCircle />Mobile App Access</li>
+                <button>Register Now</button>
+              </ul>
             </div>
+            <div className="pricing-card">
+              <h2>LEVEL 1</h2>
+              <p>$25</p>
+              <span>6 Week Package</span>
+              <ul>
+                <li><FaCheckCircle />Unlimited Gym Access</li>
+                <li><FaCheckCircle />6 Week Package</li>
+                <li><FaCheckCircle />12 - 18 Training Sessions</li>
+                <li><FaCheckCircle />Mobile App Access</li>
+                <button>Register Now</button>
+              </ul>
+            </div>
+            <div className="pricing-card">
+              <h2>LEVEL 3</h2>
+              <p>$75</p>
+              <span>6 Month Package</span>
+              <ul>
+                <li><FaCheckCircle />4 XLEVEL Assessments</li>
+                <li><FaCheckCircle />48 - 72 Training Sessions</li>
+                <li><FaCheckCircle />3 Dietitian Consultation</li>
+                <li><FaCheckCircle />Unlimited Group Classes</li>
+                <li><FaCheckCircle />Mobile App Access</li>
+                <button>Register Now</button>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
