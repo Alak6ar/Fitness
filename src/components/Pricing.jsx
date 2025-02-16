46fb448a4648861db22772693928f47aaec39fc4
Saleh545 <syusifzade545@gmail.com>
2025-02-16 17:22:20 +0400

    a

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -21,16 +21,18 @@ const Pricing = () => {
               </ul>
             </div>
             <div className="pricing-card">
-              <h2>LEVEL 1</h2>
-              <p>$25</p>
-              <span>6 Week Package</span>
+              <h2>LEVEL 2</h2>
+              <p>$55</p>
+              <span>3 Month Package</span>
               <ul>
-                <li><FaCheckCircle />Unlimited Gym Access</li>
-                <li><FaCheckCircle />6 Week Package</li>
-                <li><FaCheckCircle />12 - 18 Training Sessions</li>
+                <li><FaCheckCircle />2 XLEVEL Assessments</li>
+                <li><FaCheckCircle />24 - 36 Training Sessions</li>
+                <li><FaCheckCircle />2 Dietitian Consultation</li>
+                <li><FaCheckCircle />6 Group Classes</li>
                 <li><FaCheckCircle />Mobile App Access</li>
                 <button>Register Now</button>
               </ul>
+              <p className="best-offer">Best Offer</p>
             </div>
             <div className="pricing-card">
               <h2>LEVEL 3</h2>
